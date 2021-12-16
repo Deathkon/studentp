c5c6cfdf8132929c016501a0976502e9a2783d52
Deathkon <74615179+Deathkon@users.noreply.github.com>
2021-12-16 12:45:04 +0200

    master

--- src/main/java/com/studentp/studentp/entity/Stidentification.java
+++ src/main/java/com/studentp/studentp/entity/Stidentification.java
@@ -2,17 +2,23 @@ package com.studentp.studentp.entity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Entity
+@Table(name = "student_Records")
 public class Stidentification {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Stud_id")
     private Integer stid;
+    @Column(name = "Stud_Fname")
     private String stFname;
+    @Column(name = "Stud_Lname")
     private String stLname;
+    @Column(name = "Stud_Address")
     private String stAddress;
+    @Column(name = "Stud_Email")
     private String stEmail;
 
     public Stidentification(Integer stid, String stFname, String stLname, String stAddress, String stEmail) {
