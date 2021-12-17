73b27cab9744509902f3a781f6c32ec5ae3c930f
Deathkon <74615179+Deathkon@users.noreply.github.com>
2021-12-17 12:20:26 +0200

    commit

--- src/main/java/com/studentp/studentp/service/StudentService.java
+++ src/main/java/com/studentp/studentp/service/StudentService.java
@@ -1,13 +1,18 @@
-// package com.studentp.studentp.service;
-// import java.util.List;
+package com.studentp.studentp.service;
 
-// import com.studentp.studentp.entity.Stidentification;
+import java.util.List;
 
-// public interface StudentService {
-//     List<Stidentification> findAllStudents();
-//     Stidentification findStudentById(Integer id);
-//     Stidentification saveStudentById(Stidentification Stidentification);
-//     Stidentification updateStudentById(Stidentification Stidentification);
-//     void deleteStudentById(Integer id);
+import com.studentp.studentp.entity.Stidentification;
 
-// }
+public interface StudentService {
+    List<Stidentification> findAllStudents();
+
+    Stidentification findStudentById(Integer id);
+
+    Stidentification saveStudentById(Stidentification Stidentification);
+
+    Stidentification updateStudentById(Stidentification Stidentification);
+
+    void deleteStudentById(Integer id);
+
+}
