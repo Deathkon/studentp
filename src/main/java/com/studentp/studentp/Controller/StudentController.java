56b25905d798b461985847e7ae01db99f930c47f
Deathkon <74615179+Deathkon@users.noreply.github.com>
2021-12-17 14:10:08 +0200

    commit

--- src/main/java/com/studentp/studentp/Controller/StudentController.java
+++ src/main/java/com/studentp/studentp/Controller/StudentController.java
@@ -6,8 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import java.util.List;
-import java.util.ArrayList;
 
 @Controller
 public class StudentController {
@@ -16,9 +14,8 @@ public class StudentController {
 
     @GetMapping("/")
     public String showData(Model model) {
-        List<Stidentification> studentList = new ArrayList<>();
-        studentList = StudentRepository.findAll();
-        model.addAttribute("mList", studentList);
+        model.addAttribute("mList", StudentRepository.findAll());
+        model.addAttribute("mList", new Stidentification());
         return "index";
     }
 
