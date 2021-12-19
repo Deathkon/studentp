21a491cd105cb262908ecd70039c8060a96c1fb2
Deathkon <74615179+Deathkon@users.noreply.github.com>
2021-12-19 17:35:55 +0200

    commit

--- src/main/java/com/studentp/studentp/Controller/StudentController.java
+++ src/main/java/com/studentp/studentp/Controller/StudentController.java
@@ -5,9 +5,11 @@ import com.studentp.studentp.entity.Stidentification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class StudentController {
@@ -21,9 +23,19 @@ public class StudentController {
         return "index";
     }
     @PostMapping("/add")
-    public String addData( Stidentification student) {
+    public String addData(@ModelAttribute("student") Stidentification student) {
         StudentRepository.save(student);
         return "redirect:/";
     }
-    // @ModelAttribute("student")
+    // @PostMapping("/update")
+    // public String updateData(@ModelAttribute("student") Stidentification student) {
+    //     StudentRepository.save(student);
+    //     return "redirect:/";
+    // }
+    // @DeleteMapping("/delete")
+    // public String deleteData(@ModelAttribute("student") Stidentification student) {
+    //     StudentRepository.delete(student);
+    //     return "redirect:/";
+    // }
+
 }
