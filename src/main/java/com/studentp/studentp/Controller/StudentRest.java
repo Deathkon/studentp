ba81f6b836e276f747e0cc3e735f0591e9009277
Deathkon <74615179+Deathkon@users.noreply.github.com>
2021-12-19 16:50:08 +0200

    commit

--- src/main/java/com/studentp/studentp/Controller/StudentRest.java
+++ src/main/java/com/studentp/studentp/Controller/StudentRest.java
@@ -1,46 +1,46 @@
-// package com.studentp.studentp.Controller;
+package com.studentp.studentp.Controller;
 
-// import java.util.List;
-// import java.util.Optional;
+import java.util.List;
+import java.util.Optional;
 
-// import com.studentp.studentp.entity.Stidentification;
-// import com.studentp.studentp.service.StudentService;
+import com.studentp.studentp.entity.Stidentification;
+import com.studentp.studentp.service.StudentService;
 
-// import org.springframework.web.bind.annotation.DeleteMapping;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.PutMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-// @RestController
-// @RequestMapping("/student")
-// public class StudentRest {
+@RestController
+@RequestMapping("/student")
+public class StudentRest {
    
-//     private final StudentService studentService;
+    private final StudentService studentService;
 
-//     public StudentRest(StudentService studentService) {
-//         this.studentService = studentService;
-//     }
-//     public List<Stidentification> findAllStudents() {
-//         return studentService.findAllStudents();
-//     }
-//     @GetMapping("/{id}")
-//     public Optional<Stidentification> findStudentById(@PathVariable("stid") Integer stid) {
-//         return studentService.findStudentById(stid);
-//     }
-//     @PostMapping
-//     public Stidentification saveStudentById(@RequestBody Stidentification Stidentification) {
-//         return studentService.saveStudentById(Stidentification);
-//     }
-//     @PutMapping
-//     public Stidentification updateStudentById(@RequestBody Stidentification Stidentification) {
-//         return studentService.updateStudentById(Stidentification);
-//     }
-//     @DeleteMapping
-//     public void deleteStudentById(Integer stid) {
-//         studentService.deleteStudentById(stid);
-//     }
-// }
+    public StudentRest(StudentService studentService) {
+        this.studentService = studentService;
+    }
+    public List<Stidentification> findAllStudents() {
+        return studentService.findAllStudents();
+    }
+    @GetMapping("/{id}")
+    public Optional<Stidentification> findStudentById(@PathVariable("stid") Integer stid) {
+        return studentService.findStudentById(stid);
+    }
+    @PostMapping
+    public Stidentification saveStudentById(@RequestBody Stidentification Stidentification) {
+        return studentService.saveStudentById(Stidentification);
+    }
+    @PutMapping
+    public Stidentification updateStudentById(@RequestBody Stidentification Stidentification) {
+        return studentService.updateStudentById(Stidentification);
+    }
+    @DeleteMapping
+    public void deleteStudentById(Integer stid) {
+        studentService.deleteStudentById(stid);
+    }
+}
