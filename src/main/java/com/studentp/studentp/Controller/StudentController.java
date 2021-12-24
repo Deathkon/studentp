9983abf48a1f078145d92c40cfe5d0d425bdda49
Deathkon <74615179+Deathkon@users.noreply.github.com>
2021-12-24 10:00:41 +0200

    commit

--- src/main/java/com/studentp/studentp/Controller/StudentController.java
+++ src/main/java/com/studentp/studentp/Controller/StudentController.java
@@ -5,11 +5,11 @@ import com.studentp.studentp.entity.Stidentification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class StudentController {
